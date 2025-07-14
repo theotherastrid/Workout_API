136a77a38084a14751be8475b2d12ec2d95eb914
theotherastrid <107129643+theotherastrid@users.noreply.github.com>
2025-07-14 14:06:37 -0300

    Adding exception in centro_treinamento.py
    Adding exception handling in centro_treinamento.

--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -6,6 +6,7 @@ from workout_api.centro_treinamento.models import CentroTreinamentoModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
@@ -19,11 +20,18 @@ async def post(
     db_session: DatabaseDependency, 
     centro_treinamento_in: CentroTreinamentoIn = Body(...)
 ) -> CentroTreinamentoOut:
-    centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
-    centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
+     try:
+        centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
+        centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())  
     
     db_session.add(centro_treinamento_model)
     await db_session.commit()
+ except IntegrityError:
+        ct = centro_treinamento_in.nome
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f"Já existe uma categoria com esse nome: {ct}"
+        )
 
     return centro_treinamento_out
     
