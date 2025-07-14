fe6d33facc3b221c2f633aa54e53405ed3999416
theotherastrid <107129643+theotherastrid@users.noreply.github.com>
2025-07-14 14:03:08 -0300

    Adding exception in category.py
    Adding exception handling in category

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -6,6 +6,7 @@ from workout_api.categorias.models import CategoriaModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 
 router = APIRouter()
 
@@ -19,12 +20,18 @@ async def post(
     db_session: DatabaseDependency, 
     categoria_in: CategoriaIn = Body(...)
 ) -> CategoriaOut:
-    categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
-    categoria_model = CategoriaModel(**categoria_out.model_dump())
-    
-    db_session.add(categoria_model)
-    await db_session.commit()
+   try:
+        categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
+        categoria_model = CategoriaModel(**categoria_out.model_dump())
 
+        db_session.add(categoria_model)
+        await db_session.commit()
+    except IntegrityError:
+        categoria = categoria_in.nome
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f"Já existe uma categoria com esse nome: {categoria}"
+        )
     return categoria_out
     
     
